bd7bc63cae4d1403b9cb36759f38771e86ae1c18
Daniel Gomez <134628127+Hurin70@users.noreply.github.com>
2026-06-28 13:10:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import asyncio
+import pygame
 import Juego
 
-asyncio.run(Juego.main())
+async def main():
+    # Inicializa el motor de juego dentro del entorno web
+    pygame.init()
+    
+    # Llama a la función principal de tu juego de manera asíncrona
+    await Juego.main()
+
+# Punto de entrada obligatorio para el emulador web de pygbag
+asyncio.run(main())
